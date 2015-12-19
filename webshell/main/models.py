fadb50f3417e3978f9ec3c448d4a6626cf9728bb
Josh Albrecht <thejash@gmail.com>
2015-12-18 16:57:25 -0800

    second deploy

--- webshell/main/models.py
+++ webshell/main/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class AWSTestModel(models.Model):
     random_text = models.CharField(max_length=200)
+    new_field = models.CharField(max_length=600, default="some more data")
